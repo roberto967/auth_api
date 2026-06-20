7e34509177ad489ab48d779609f7072e3cb11a8a
Roberto <robertoo.jof@gmail.com>
2026-06-20 17:50:15 -0300

    feat: refatora middleware de documentação Swagger para incluir tipos de requisição e resposta

--- auth_api/src/modules/v1/index.ts
+++ auth_api/src/modules/v1/index.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { RegisterRoutes as RegisterRoutesV1 } from '../../generated/v1/routes';
 import swaggerUi, { JsonObject } from 'swagger-ui-express';
 import * as fs from 'fs';
@@ -6,14 +6,19 @@ import * as path from 'path';
 
 const swaggerPath = path.resolve(__dirname, '../../generated/v1/swagger.json');
 
-const swaggerDoc = JSON.parse(
-  fs.readFileSync(swaggerPath, 'utf8'),
-) as JsonObject;
-
 const router = Router();
 
 RegisterRoutesV1(router);
 
-router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+router.use(
+  '/api-docs',
+  swaggerUi.serve,
+  (req: Request, res: Response, next: NextFunction) => {
+    const swaggerDoc = JSON.parse(
+      fs.readFileSync(swaggerPath, 'utf8'),
+    ) as JsonObject;
+    swaggerUi.setup(swaggerDoc)(req, res, next);
+  },
+);
 
 export default router;
